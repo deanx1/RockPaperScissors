397a61f15b67cdd0e9376bcf06cd40b0b5a31ca9
Dean van Doorn <dean@code-orange.nl>
2019-11-08 22:53:21 +0100

    fix layout 2

--- app/src/main/java/com/androidcourse/shoppinglistkotlin/ui/ProductAdapter.kt
+++ app/src/main/java/com/androidcourse/shoppinglistkotlin/ui/ProductAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.androidcourse.shoppinglistkotlin.model.Product
 import com.androidcourse.shoppinglistkotlin.R
+import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.item_product.view.*
 
 class ProductAdapter(private val products: List<Product>) : RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
@@ -18,12 +19,56 @@ class ProductAdapter(private val products: List<Product>) : RecyclerView.Adapter
 
     override fun getItemCount(): Int = products.size
 
+//    , you chose paper.
+    private fun setImage(play :String) :Int {
+    if (play.contains("rock")) {
+        return 1
+    }
+    if (play.contains("paper")) {
+        return 2
+    }
+        return 3
+    }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(products[position])
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(product: Product) {
-            itemView.tvProduct.text = product.whowon + product.computerplay + product.yourplay
+
+            var imgRock = R.drawable.rock
+            var imgPaper = R.drawable.paper
+            var imgScissors = R.drawable.scissors
+
+            val Strings = arrayOf(imgRock, imgPaper, imgScissors)
+
+//            itemView.tvProduct.text = product.whowon + product.computerplay + product.yourplay
+//            itemView.tvProduct.text = product.whowon + product.computerplay + product.yourplay
+            itemView.tvWhoWon.text = product.whowon
             itemView.tvDate.text = product.date
+
+            var computerPlay = setImage(product.computerplay)
+            var yourPlay = setImage(product.yourplay)
+//            itemView.ivComputerHistory.setImageResource(R.drawable.rock)
+
+//            itemView.ivComputerHistory.setImageResource(Strings[computerPlay])
+//            itemView.ivYouHistory.setImageResource(Strings[yourPlay])
+
+            if (computerPlay == 1) {
+                itemView.ivComputerHistory.setImageResource(R.drawable.rock)
+            } else if (computerPlay == 2) {
+                itemView.ivComputerHistory.setImageResource(R.drawable.paper)
+            } else {
+                itemView.ivComputerHistory.setImageResource(R.drawable.scissors)
+            }
+
+            if (yourPlay == 1) {
+                itemView.ivYouHistory.setImageResource(R.drawable.rock)
+            } else if (computerPlay == 2) {
+                itemView.ivYouHistory.setImageResource(R.drawable.paper)
+            } else {
+                itemView.ivYouHistory.setImageResource(R.drawable.scissors)
+            }
+
         }
     }
 }
